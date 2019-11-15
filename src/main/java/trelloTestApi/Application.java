37a4601a6a6d1a880034c24a84ef3e6283a130d7
Marinos <mpetratos@dynasource.com>
2019-11-15 11:42:16 +0100

    changed text to command liner

--- src/main/java/trelloTestApi/Application.java
+++ src/main/java/trelloTestApi/Application.java
@@ -16,7 +16,7 @@ public class Application {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            System.out.println(FigletFont.convertOneLine("FUCK OFF!!"));
+            System.out.println(FigletFont.convertOneLine("API TRELLO TEST"));
         };
     }
 
